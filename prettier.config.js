f365d7c714faa5ecac69cdb0053f03579c361cff
LieuNyan <lieu3376@gmail.com>
2025-11-25 12:05:39 +0900

    chore: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,19 @@
-//  @ts-check
+// @ts-check
 
 /** @type {import('prettier').Config} */
 const config = {
-  semi: false,
+  printWidth: 100,
+  tabWidth: 2,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  semi: false,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
 };
 
 export default config;
