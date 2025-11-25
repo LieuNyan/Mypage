3f7a0d79ee82c18427964d8709109d3edb39e086
LieuNyan <lieu3376@gmail.com>
2025-11-25 17:28:25 +0900

    feat: scroll to section on tab click

--- src/routes/(main)/_components/body.tsx
+++ src/routes/(main)/_components/body.tsx
@@ -1,5 +1,5 @@
-import { Fragment } from 'react/jsx-runtime'
 import { MainTabs, type MainTab } from './_tabs'
+import { useEffect, useRef } from 'react'
 
 export default function MainBody({
   activeTab,
@@ -8,10 +8,31 @@ export default function MainBody({
   activeTab: MainTab
   setActiveTab: (tab: MainTab) => void
 }) {
+  const tabRefs = useRef<(HTMLElement | null)[]>([])
+
+  useEffect(() => {
+    const index = MainTabs.findIndex((t) => t.href === activeTab.href)
+    const el = tabRefs.current[index]
+
+    if (el) {
+      el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      })
+    }
+  }, [activeTab])
+
   return (
     <div className='container mx-auto mt-[56px] flex flex-auto flex-col space-y-5 overflow-y-auto px-4'>
-      {MainTabs.map((tab) => (
-        <Fragment key={tab.href}>{tab.component}</Fragment>
+      {MainTabs.map((tab, index) => (
+        <section
+          key={tab.href}
+          ref={(el) => {
+            tabRefs.current[index] = el
+          }}
+        >
+          {tab.component}
+        </section>
       ))}
     </div>
   )
